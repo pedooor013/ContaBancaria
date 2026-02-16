1ea370bdfe25f5d57bf061ae50ffc2752666cb57
Pedro Lopes <pedrolopes.programador013@gmail.com>
2026-02-16 11:00:34 -0300

    fix: finalização do projeto, usando conceitos de POO e tentando aplicar o Clean Code

--- ContaBancaria/ContaBancaria.cs
+++ ContaBancaria/ContaBancaria.cs
@@ -2,5 +2,51 @@
 
 public class ContaBancaria
 {
+    public string NumeroConta { get; private set; }
+    private decimal _saldo;
+    public string NomeTitular { get; private set; }
+
+    public void RealizarDeposito(decimal valor)
+    {
+        if (valor <= 0)
+        {
+            Console.WriteLine("Valor inválido para deposíto!");
+            return;
+        }
+
+        _saldo += valor;
+    }
+
+    public void RealizarSaque(decimal valor)
+    {
+        if (valor > _saldo)
+        {
+            Console.WriteLine("Saldo insuficiente!");
+            return;
+        }else if (valor <= 0)
+        {
+            Console.WriteLine("Valor de saque inválido!");
+            return;
+        }
+        
+        _saldo -= valor;
+    }
+
+    public decimal ExibirSaldo()
+    {
+        return _saldo;
+    }
     
+    //Construtor
+    public ContaBancaria(string numeroConta, string nomeTitular)
+    {
+        if (string.IsNullOrWhiteSpace(numeroConta))
+            throw new ArgumentException("Número da conta inválido.");
+
+        if (string.IsNullOrWhiteSpace(nomeTitular))
+            throw new ArgumentException("Nome do titular inválido.");
+        
+        NumeroConta = numeroConta;
+        NomeTitular = nomeTitular;
+    }
 }
--- ContaBancaria/Program.cs
+++ ContaBancaria/Program.cs
@@ -1,2 +1,139 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        public static List<ContaBancaria> contas = new List<ContaBancaria>();
+
+        static void Main(string[] args)
+        {
+            LogicaExibicaoMenu(contas);
+        }
+        
+
+        static void ExibirMenuSemConta()
+        {
+            Console.WriteLine("O que deseja fazer: " +
+                              "\n01) Cadastrar conta;" +
+                              "\n02) Sair;\n");
+            int escolhaUser =  Convert.ToInt32(Console.ReadLine());
+            EscolhaSemConta(escolhaUser);
+        }
+
+        static void ExibirMenuComConta()
+        {
+            Console.WriteLine("O que deseja fazer:" +
+                              "\n01) Exibir informações da conta;" +
+                              "\n02) Exibir o saldo;" +
+                              "\n03) Depositar;" +
+                              "\n04) Sacar;" +
+                              "\n05) Sair;\n");
+            int escolhaUser = Convert.ToInt32(Console.ReadLine());
+            EscolhaComConta(escolhaUser);
+        }
+
+        private static void LogicaExibicaoMenu(List<ContaBancaria> contas)
+        {
+            if (contas.Count == 0)
+            {
+                ExibirMenuSemConta();
+            }
+            else
+            {
+                ExibirMenuComConta();
+            }
+        }
+
+        static void EscolhaSemConta(int escolha)
+        {
+            switch (escolha)
+            {
+                case 1:
+                    CriarConta();
+                    break;
+                case 2:
+                    Console.WriteLine("Saindo...");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida! Digite novbamente!");
+                    LogicaExibicaoMenu(contas);
+                    break;
+            }
+        }
+
+        static void CriarConta()
+        {
+            Console.WriteLine("Digite o numero da conta: ");
+            string numeroConta = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome do titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+            
+            contas.Add(new ContaBancaria(numeroConta,  nomeTitular));
+            LogicaExibicaoMenu(contas);
+        }
+
+        static void EscolhaComConta(int escolha)
+        {
+            switch (escolha)
+            {
+                case 1:
+                    ExibirInformacoes();
+                    break;
+                case 2:
+                    ExibirSaldo();
+                    break;
+                case 3:
+                    RealizarDeposito();
+                    break;
+                case 4:
+                    RealizarSaque();
+                    break;
+                case 5:
+                    Console.WriteLine("Saindo...");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida! Digite novamente!");
+                    LogicaExibicaoMenu(contas);
+                    break;
+            }
+        }
+        
+        static void ExibirInformacoes()
+        {
+            Console.WriteLine($"Informações da conta:" +
+                              $"\nNome: {contas[0].NomeTitular}" +
+                              $"\nNº Conta: {contas[0].NumeroConta}" +
+                              $"\nSaldo: {contas[0].ExibirSaldo().ToString("C")}\n");
+            LogicaExibicaoMenu(contas);
+        }
+
+        static void ExibirSaldo()
+        {
+            Console.WriteLine($"Saldo em conta: {contas[0].ExibirSaldo().ToString("C")}\n");
+            LogicaExibicaoMenu(contas);
+        }
+
+        static void RealizarDeposito()
+        {
+            Console.WriteLine("Digite o valor para deposito: ");
+            decimal valor = decimal.Parse(Console.ReadLine());
+            
+            contas[0].RealizarDeposito(valor);
+            LogicaExibicaoMenu(contas);
+        }
+
+        static void RealizarSaque()
+        {
+            Console.WriteLine("Digite o valor para saque: ");
+            decimal valor = decimal.Parse(Console.ReadLine());
+            
+            contas[0].RealizarSaque(valor);
+            LogicaExibicaoMenu(contas);
+        }
+        
+    }
+}
